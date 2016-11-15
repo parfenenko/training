a754ea5cbee5b913309a854fa25f9cca3cec3adb
irmar <irmar@ciklum.com>
2016-11-15 15:22:49 +0200

    Project commit

--- IKostiukova/src/main/java/com/ciklum/com/Main.java
+++ IKostiukova/src/main/java/com/ciklum/com/Main.java
@@ -1,7 +1,7 @@
-package com.ciklum.workshops;
+package com.ciklum.com;
 
 /**
- * Created by irmar on 11/14/2016.
+ * Created by irmar on 11/15/2016.
  */
 public class Main {
     public static void main(String[] args) {
