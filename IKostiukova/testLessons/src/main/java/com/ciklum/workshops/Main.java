406cd77010570a2d6a63fc324884810e0ca9fa67
irmar <irmar@ciklum.com>
2016-11-15 14:13:51 +0200

    TestLessons project commit

---
+++ IKostiukova/testLessons/src/main/java/com/ciklum/workshops/Main.java
@@ -0,0 +1,10 @@
+package com.ciklum.workshops;
+
+/**
+ * Created by irmar on 11/14/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Iryna Kostiukova");
+    }
+}
