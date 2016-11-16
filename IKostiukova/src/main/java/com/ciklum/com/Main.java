9e225a08b09319228d413f9c374ab54aca87919b
sepi <sepi@ciklum.com>
2016-11-16 12:54:01 +0200

    merge

---
+++ IKostiukova/src/main/java/com/ciklum/com/Main.java
@@ -0,0 +1,10 @@
+package com.ciklum.com;
+
+/**
+ * Created by irmar on 11/15/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello from Iryna Kostiukova");
+    }
+}
